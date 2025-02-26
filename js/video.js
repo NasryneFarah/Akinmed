79c4ee6dad71ab7ef60cda0496c2f65bf91bfddc
Farah KONE <farahyedidiah@gmail.com>
2025-02-26 03:54:31 +0000

    ajout de fav ico sur les pages et correction de la page actualite lorsque l'utilisateur arrive sur la page le webinaire est bien caché

--- js/video.js
+++ js/video.js
@@ -1,32 +1,34 @@
 $(document).ready(function () {
-    // Ouvrir le modal
-    function openModal() {
-        $("#videoModal").fadeIn(500);;
-    }
+  // Vérifier si le modal s'ouvre dès le chargement
+  $("#videoModal").hide(); // S'assurer qu'il est caché au démarrage
+  //Ouvrir le modal
+  function openModal() {
+    $("#videoModal").fadeIn(500);
+  }
 
-    // Fermer le modal et arrêter la vidéo
-    function closeModal() {
-        $("#videoModal").fadeOut(500, function() {
-            // Arrêter la vidéo à la fermeture du modal
-            var iframe = $("#videoModal iframe");
-            var src = iframe.attr("src");
-            iframe.attr("src", "");  // Stopper la vidéo
-            iframe.attr("src", src); // Relancer la vidéo si besoin lors de l'ouverture suivante
-        });
-    }
-
-    // Gestion de l'ouverture du modal
-    $(".card-btn").on("click", function () {
-        openModal();
+  // Fermer le modal et arrêter la vidéo
+  function closeModal() {
+    $("#videoModal").fadeOut(500, function () {
+      // Arrêter la vidéo à la fermeture du modal
+      var iframe = $("#videoModal iframe");
+      var src = iframe.attr("src");
+      iframe.attr("src", ""); // Stopper la vidéo
+      iframe.attr("src", src); // Relancer la vidéo si besoin lors de l'ouverture suivante
     });
+  }
 
-    // Gestion du clic sur le bouton de fermeture du modal
-    $(".close").on("click", function () {
-        closeModal();
-    });
+  // Gestion de l'ouverture du modal
+  $(".card-btn").on("click", function () {
+    openModal();
+  });
 
-    // Fermer le modal si l'utilisateur change de page
-    $(window).on("beforeunload", function () {
-        closeModal();
-    });
+  // Gestion du clic sur le bouton de fermeture du modal
+  $(".close").on("click", function () {
+    closeModal();
+  });
+
+  // Fermer le modal si l'utilisateur change de page
+  $(window).on("pagehide", function () {
+    closeModal();
+  });
 });
