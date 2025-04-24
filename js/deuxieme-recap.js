e0a0adffb78bb3d3b0d0e369d24c0ce53e1ad37c
Farah KONE <farahyedidiah@gmail.com>
2025-04-24 11:25:23 +0000

    ajout du deuxième webinaire

---
+++ js/deuxieme-recap.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // Redirection vers l'inscription au webinaire
+    $(".voir-recap").on("click", function () {
+        window.location.href = "https://akin22-my.sharepoint.com/:v:/g/personal/imelda_niangne_akinmed_fr/Ed3KuXY9-eVFs9OW5XCv17QBocbLX3qU0HgFKcjQ3ctifw";
+    });
+  });
--- js/inscription.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    // Redirection vers l'inscription au webinaire
-    $(".inscription-btn").on("click", function () {
-        window.location.href = "https://us06web.zoom.us/meeting/register/aBlcU9GxTAKwUc5Xt0kEww";
-    });
-  });
